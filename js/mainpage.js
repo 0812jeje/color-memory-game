7fc333ae339183a52cf20728dde49a047842f4c7
Jason Iloppa <iloppajason@icloud.com>
2024-06-04 22:08:19 +0200

    added main game

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,18 +1,44 @@
+const Buttons = {
+  GREEN: "blue-btn",
+  RED: "blue-btn",
+  YELLOW: "blue-btn",
+  BLUE: "blue-btn",
+};
+
 function startGame() {
   document.getElementById("overlay").style.display = "flex";
   let countdownElement = document.getElementById("countdown");
-  let countdown = 3; // countdown from 3 seconds
+  let countdown = 3;
 
   let interval = setInterval(() => {
     countdown--;
     countdownElement.textContent = countdown;
 
-    if (countdown <= 0) {
-      clearInterval(interval);
-      countdownElement.textContent = countdown;
-      document.getElementById("overlay").style.display = "none";
-      // You can add additional logic here to start the game
-      window.open("gamepage.html", "_self");
+    if (countdown == 0) {
+      countdownElement.textContent = "START";
     }
+
+    if (countdown < 0) {
+      clearInterval(interval);
+      document.getElementById("overlay").style.display = "none";
+      document
+        .querySelectorAll(".mainpage")
+        .forEach((item) => (item.style.display = "none"));
+      inGame();
+    }
+  }, 1000);
+}
+
+function inGame() {
+  const buttons = document.querySelectorAll(".circle-btn");
+  let counter = 0;
+  let buttonOrder = [];
+  let interval1 = setInterval(() => {
+    buttonOrder.push(buttons[Math.floor(Math.random() * 4)]);
+    let i = 0;
+
+    let interval2 = setInterval(() => {
+      buttonOrder[i].style.backgroundColor = "black";
+    }, 500);
   }, 1000);
 }
