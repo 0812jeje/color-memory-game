78f40d91a182c58a8cc910fe35b2eb4d94810d02
Jason Iloppa <iloppajason@icloud.com>
2024-06-06 09:41:29 +0200

    game over screen
    added the game over screen and gave it some functionality

--- mainpage.js
+++ mainpage.js
@@ -31,30 +31,25 @@ async function startGame() {
 async function game() {
   let buttonList = document.querySelectorAll(".circle-btn");
   let buttonOrder = [];
+  let lost = false;
 
-  while (true) {
+  while (!lost) {
     let lastButton = buttonList[0];
     let lastButtonColor = lastButton.style.backgroundColor;
     buttonOrder.push(buttonList[Math.floor(Math.random() * 4)]);
+    document.getElementById("heading").textContent = "Watch closely";
+    await sleep(1);
     for (let index = 0; index < buttonOrder.length; index++) {
       lastButton.style.backgroundColor = lastButtonColor;
       lastButton = buttonOrder[index];
       lastButtonColor = lastButton.style.backgroundColor;
       buttonOrder[index].style.backgroundColor = "black";
-      await sleep(1);
+      await sleep(0.8);
     }
     lastButton.style.backgroundColor = lastButtonColor;
 
-    await sleep(0.1);
-    let overlayText = document.getElementById("overlay-text");
-    document.getElementById("overlay").style.display = "flex";
-    overlayText.textContent = "YOUR TURN";
-    await sleep(1);
-    document.getElementById("overlay").style.display = "none";
-
-    // buttonList.forEach((button) => {
-    //   button.addEventListener("click", handleClick);
-    // });
+    await sleep(0.5);
+    document.getElementById("heading").textContent = "Your turn";
 
     for (let index = 0; index < buttonOrder.length; index++) {
       const event = await waitForClick(); // Wait for the 'click' event
@@ -65,8 +60,29 @@ async function game() {
         console.log("Correct element clicked!");
       } else {
         console.log("Incorrect element clicked.");
+        lost = true;
+        break;
       }
     }
-    await sleep(1);
+    if (!lost) {
+      await sleep(0.5);
+      document.getElementById("score").textContent =
+        "SCORE: " + buttonOrder.length;
+      if (buttonOrder.length) {
+        document.getElementById("score").style.display = "flex";
+      }
+    } else {
+      break;
+    }
+    await sleep(0.5);
+  }
+  document.getElementById("game-over-btns").style.display = "flex";
+  document.getElementById("overlay-text").textContent = "GAME OVER";
+  document.getElementById("overlay").style.display = "flex";
+  const event = await waitForClick();
+  if (event.target.className === "home game-over-btn") {
+    location.reload();
+  } else if (event.target.className === "again game-over-btn") {
+    document.getElementById("overlay").style.display = "none";
   }
 }
